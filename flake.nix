817d13c08f4707fcd8cde9d3b91b1cfd420317d6
r2r-dev <39031033+r2r-dev@users.noreply.github.com>
2023-03-22 22:33:32 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+    src.url = "https://registry.npmjs.org/b3-strace-parser/-/b3-strace-parser-0.3.0.tgz";
+    src.flake = false;
+  };
+
+  outputs = {
+    self,
+    dream2nix,
+    src,
+  } @ inp:
+    (dream2nix.lib.makeFlakeOutputs {
+      systems = ["x86_64-linux"];
+      config.projectRoot = ./.;
+      source = src;
+      settings = [
+        {
+          subsystemInfo.noDev = true;
+          subsystemInfo.nodejs = 18;
+        }
+      ];
+    })
+    // {
+      checks = self.packages;
+    };
+}
